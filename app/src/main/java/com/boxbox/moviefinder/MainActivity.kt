fe5bd0fc1ed047098cfc6ea50c745ed57988eb4f
RafaelLibrero <librerorafael@outlook.com>
2025-06-04 16:44:20 +0200

    First commit

---
+++ app/src/main/java/com/boxbox/moviefinder/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.boxbox.moviefinder
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun getRetrofit() : Retrofit {
+        return Retrofit
+            .Builder()
+            .baseUrl("http://www.omdbapi.com/?apikey=d0b8654&")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
